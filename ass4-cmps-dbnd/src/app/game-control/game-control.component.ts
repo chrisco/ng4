8ea8f2fca174205b1845d40a71444f48f653c133
Christopher Comella <git.chrisco@gmail.com>
2017-04-19 11:42:48 -0400

    WIP: Ass 4 modified:   src/app/app.component.html modified:   src/app/game-control/game-control.component.html modified:   src/app/game-control/game-control.component.ts

--- ass4-cmps-dbnd/src/app/game-control/game-control.component.ts
+++ ass4-cmps-dbnd/src/app/game-control/game-control.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-control.component.css']
 })
 export class GameControlComponent implements OnInit {
+  myInterval;
+  myNum = 1;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  startGame() {
+    console.log('Game on');
+    this.myInterval = setInterval(
+      () => {
+        console.log(this.myNum);
+        this.myNum++;
+      },
+      1000
+    );
   }
 
+  endGame() {
+    clearInterval(this.myInterval);
+    console.log('Game off');
+  }
 }
