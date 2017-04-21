c4f6901455594c299841bd3b2ee955256825c5ac
Christopher Comella <git.chrisco@gmail.com>
2017-04-21 07:43:35 -0400

    ngFor and ngIf Recap

--- 7-directives-start/src/app/app.component.ts
+++ 7-directives-start/src/app/app.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  numbers = [1, 2, 3, 4, 5];
+  // numbers = [1, 2, 3, 4, 5];
+  oddNumbers = [1, 3, 5];
+  evenNumbers = [2, 4];
   onlyOdd = false;
 }
