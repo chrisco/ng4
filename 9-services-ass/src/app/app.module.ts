25528a7bb820daa40d80b36897dfd917e6bc7419
Christopher Comella <git.chrisco@gmail.com>
2017-04-25 07:47:34 -0400

    Start 9: Ass 5: Practice Services

---
+++ 9-services-ass/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { ActiveUsersComponent } from './active-users/active-users.component';
+import { InactiveUsersComponent } from './inactive-users/inactive-users.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ActiveUsersComponent,
+    InactiveUsersComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
