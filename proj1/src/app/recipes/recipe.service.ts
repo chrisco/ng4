8c9b749cf94cb8ab989516a2077c0d5748c0e49c
Christopher Comella <git.chrisco@gmail.com>
2017-04-28 21:49:55 -0400

    Add Ingredients to Recipes

--- proj1/src/app/recipes/recipe.service.ts
+++ proj1/src/app/recipes/recipe.service.ts
@@ -1,6 +1,7 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
@@ -9,12 +10,24 @@ export class RecipeService {
     new Recipe(
       'Pad Thai',
       'Pad thai or phad thai is a stir-fried rice noodle dish commonly served as a street food and at casual local eateries in Thailand.',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Phat_Thai_kung_Chang_Khien_street_stall.jpg/768px-Phat_Thai_kung_Chang_Khien_street_stall.jpg'
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Phat_Thai_kung_Chang_Khien_street_stall.jpg/768px-Phat_Thai_kung_Chang_Khien_street_stall.jpg',
+      [
+        new Ingredient('Rice noodles', 1),
+        new Ingredient('Chicken', 1),
+        new Ingredient('Veggies', 1),
+        new Ingredient('Seasonings', 1)
+      ]
     ),
     new Recipe(
       'Chicken Kathi Roll',
       'Boneless chicken pieces sautéed with ginger garlic, cumin, onion and chillies - stuffed into a refined flour roti slathered with fresh green chutney and sprinkled with chaat masala and more onions.',
-      'http://images.tastespotting.com/uploads/thumbnail/50726.jpg'
+      'http://images.tastespotting.com/uploads/thumbnail/50726.jpg',
+      [
+        new Ingredient('Pack of roti', 10),
+        new Ingredient('Chicken', 1),
+        new Ingredient('Veggies', 1),
+        new Ingredient('Seasonings', 1)
+      ]
     )
   ];
 
