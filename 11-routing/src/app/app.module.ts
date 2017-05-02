fc65230e36bb8a1dd7e806e4be6efff79bfd8d5a
Christopher Comella <git.chrisco@gmail.com>
2017-05-01 20:02:32 -0400

    Routing practice and gotchas

--- 11-routing/src/app/app.module.ts
+++ 11-routing/src/app/app.module.ts
@@ -23,6 +23,10 @@ const appRoutes: Routes = [
     component: ServersComponent
   },
   {
+    path: 'servers/:id',
+    component: ServerComponent
+  },
+  {
     path: 'servers/:id/edit',
     component: EditServerComponent
   },
--- 11-routing/src/app/servers/server/server.component.ts
+++ 11-routing/src/app/servers/server/server.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -8,12 +9,18 @@ import { ServersService } from '../servers.service';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
+  server: { id: number, name: string, status: string };
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    const id = +this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.server = this.serversService.getServer(+params['id']);
+      }
+    );
   }
-
 }
