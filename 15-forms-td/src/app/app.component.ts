ed21787b8e7e6c8b7722e5100ca9e563dcdf495b
Christopher Comella <git.chrisco@gmail.com>
2017-05-12 17:00:37 -0400

    Demo: Register form contrls, submit, access data

--- 15-forms-td/src/app/app.component.ts
+++ 15-forms-td/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+    console.log(form.form.value);
   }
 }
