07853f6494bba22fd51ef64ab4b322885637fa9a
Christopher Comella <git.chrisco@gmail.com>
2017-06-20 13:20:35 +0200

    Animations Triggers and State

--- 23-animations/src/app/app.component.ts
+++ 23-animations/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  trigger,
+  state,
+  style
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('divState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0)'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px)'
+      }))
+    ])
+  ]
 })
 export class AppComponent {
+  state = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
   onAdd(item) {
